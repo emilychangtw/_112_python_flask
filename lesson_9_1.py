876fad35a0fa87290a56b906e0c648bb1dc6c5f4
emilychangtw <chang12038264@yahoo.com.tw>
2023-06-03 11:59:53 +0800

    modufy

---
+++ lesson_9_1.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.write("#### Hello world Robert")
+st.write("Hello ***徐國堂***")
+
+#下面是magic的寫法
+
+"#### Hello world Robert"
+"Hello ***徐國堂***"
+
+st.markdown("Hello! **world**!")
+st.divider()
+st.title("這是app的title")
+st.divider()
+st.header("這是header")
+st.divider()
+st.subheader("這是subheader")
+st.divider()
+st.caption("這是caption")
+st.divider()
+st.code("a=123")
